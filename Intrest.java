a3e8534dc868c747095f02bfc1c804d00eecc99f
sunayana2000 <sunayanakatha@gmail.com>
2020-02-17 22:12:45 +0530

    Intrest

---
+++ Intrest.java
@@ -0,0 +1,20 @@
+
+package newyeargift;
+import java.util.*;
+public class Intrest {
+    public static void main(String[] args) {
+        double pr,rate,t,sim,com;
+        Scanner sc= new Scanner(System.in);
+         System.out.println("Enter the amount : ");
+         pr = sc.nextDouble();
+          System.out.println("Enter the number of years");
+          t= sc.nextDouble();
+           System.out.println("Enter the rate of intrest");
+           rate = sc.nextDouble();
+           sim = (pr*t*rate)/100;
+           com=pr * Math.pow(1.0+rate/100.0,t)-pr;
+            System.out.println("Simple Intrest = "+sim);
+             System.out.println("Compound Intrest = "+com);
+    }
+    
+}
